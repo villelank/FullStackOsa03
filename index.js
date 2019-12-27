4a32a04789fb8df0ecf172c40870b3d102832a11
Ville Lankinen <villelank@gmail.com>
2019-12-27 22:52:56 +0200

    projekti luotu, tehtävät 3.1 ja 3.2 tehty

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const app = express()
+
+let persons = [
+  { name: 'Arto Hellas', 
+    number: '040-123456',
+    id: 1
+  },
+  { name: 'Ada Lovelace', 
+    number: '39-44-5323523',
+    id: 2
+  },
+  { name: 'Dan Abramov', 
+    number: '12-43-234345',
+    id: 3 
+  },
+  { name: 'Mary Poppendieck', 
+    number: '39-23-6423122',
+    id: 4
+  }
+]
+
+const dateNow = Date()
+
+let info = `<p>Phonebook has info for ${persons.length} people</p><p>${dateNow}</p>`
+
+app.get('/api/persons', (req, res) => {
+  res.json(persons)
+})
+
+app.get('/info', (req, res) => {
+  res.send(info)
+})
+
+const port = 3001
+app.listen(port, () => {
+  console.log(`Server running on ${port}`)
+})
