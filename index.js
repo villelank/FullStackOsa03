8ec28a5dcdad121b126d27378cd8ddffb2c65afb
Ville Lankinen <villelank@gmail.com>
2019-12-28 01:25:42 +0200

    3.5 ja 3.6 tehty

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.json())
 
 let persons = [
   { name: 'Arto Hellas', 
@@ -46,6 +49,49 @@ app.delete('/api/persons/:id', (req, res) => {
   const id = Number(req.params.id)
   persons = persons.filter(person => person.id !== id)
   res.status(204).end()
+})
+
+const createNewId = () => {
+  return newId = Math.floor(Math.random() * Math.floor(1000000))
+}
+
+app.post('/api/persons', (req, res) => {
+  
+  const body = req.body
+  
+  if( !body.name ) {
+    console.log('name is missing')
+    return res.status(400).json({
+      error: 'name is missing'
+    })
+  }
+
+  if( !body.number ) {
+    console.log('number is missing')
+    return res.status(400).json({
+      error: 'number is missing'
+    })
+  }
+
+  const samePerson = persons.find(person => person.name === body.name)
+  if(samePerson) {
+    console.log('name must be unique')
+    return res.status(400).json({
+      error: 'name must be unique'
+    })
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: createNewId()
+  }
+
+  persons = persons.concat(person)
+
+  console.log(person)
+
+  res.json(person)
 })
 
 const port = 3001
